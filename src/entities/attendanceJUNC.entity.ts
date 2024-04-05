ed38d7d1d06faecafdc7dac3c7af24dcb4566556
정권권/선임연구원/TV SW CI팀(isu.jeong@lge.com) <isu.jeong@lge.com>
2024-04-05 16:49:21 +0900

    merge

--- src/entities/attendanceJUNC.entity.ts
+++ src/entities/attendanceJUNC.entity.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // src/entities/attendanceJUNC.entity.ts
-=======
-// src/entities/attendance.entity.ts
->>>>>>> a73401d1f5efcc9523afe315ee9bb98739c79ebb
 // The entity class for the attendance JUNCTION table
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { AttendanceDate } from './attendance-date.entity';
@@ -16,32 +12,22 @@ export class AttendanceJUNC {
   id: number;
 
   // Attendance relationship with Person
-<<<<<<< HEAD
   @Column({ name: 'person_id' })
   person_id: number;
-=======
->>>>>>> a73401d1f5efcc9523afe315ee9bb98739c79ebb
   @ManyToOne(() => Person, person => person.attendances)
   @JoinColumn({ name: 'person_id' })
   person: Person;
 
   // Attendance relationship with RoleJUNC
-<<<<<<< HEAD
   @Column({ name: 'role_id' })
   role_id: number;
-=======
->>>>>>> a73401d1f5efcc9523afe315ee9bb98739c79ebb
   @ManyToOne(() => RoleJUNC, roleJUNC => roleJUNC.attendances)
   @JoinColumn({ name: 'role_id' })
   roleJUNC: RoleJUNC;
 
-<<<<<<< HEAD
   // Attendance relationship with AttendanceDate
   @Column({ name: 'date_id' })
   date_id: number;
-=======
-    // Attendance relationship with AttendanceDate
->>>>>>> a73401d1f5efcc9523afe315ee9bb98739c79ebb
   @ManyToOne(() => AttendanceDate, attendanceDate => attendanceDate.attendances)
   @JoinColumn({ name: 'date_id' })
   attendance_date: AttendanceDate;
